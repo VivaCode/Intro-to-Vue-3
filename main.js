2da7fcaf993380e9b615715dec8aa22e7fed0840
Emily McClanahan <efmcclanahan@gmail.com>
2021-03-01 12:30:06 -0600

    lesson and challen work included

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const app = Vue.createApp({
 			product: "Socks",
 			brand: "Vue Mastery",
 			description: "Fuzzy, warmess for your toes! Available in 2 colors.",
-			image: "./assets/images/socks_green.jpg",
+			selectedVariant: 0,
 			imageAlt: "a pair of socks, in selected color",
 			url: "http://www.google.com",
 			inventory: 10,
 			onSale: false,
 			details: ["50% cotton", "30% wool", "20% polyester"],
 			variants: [
-				{ id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-				{ id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+				{ id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+				{ id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 },
 			],
 			sizes: [
 				{ id: 00, size: "xs" },
@@ -31,8 +31,21 @@ const app = Vue.createApp({
 		removeFromCart() {
 			this.cart -= 1;
 		},
-		updateImage(variantImage) {
-			this.image = variantImage;
+		updateVariant(index) {
+			this.selectedVariant = index;
+		},
+	},
+	computed: {
+		title() {
+			if (this.onSale) {
+				return this.brand + " " + this.product + " is on Sale!";
+			} else return this.brand + " " + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].image;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].quantity;
 		},
 	},
 });
